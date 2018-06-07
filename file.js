e76583e27fcf7c4ae05a989ed93820f15b2a060a
saiyamkumar13 <saiyamkumar13@gmail.com>
2018-06-07 07:55:24 -0400

    mod6

--- file.js
+++ file.js
@@ -1,15 +1,17 @@
 $(document).ready(function() { 
    $("#btn").click(function(){ 
-          
-    // $("#Tab").empty();
+         
+     
+   $("tbody").empty();
+    $("#tab").append(); 
 $.getJSON("Aggfilm.php", function(dati){         
-
+var ta=$("<table id='Tab' align='center' class='table table-dark'><thead class='thead-dark'></thead><tbody align='center'><tr><td><h4>Nome :</h4></td><td><h4>Posti :</h4></td><td><h4>Città :</h4></td></tr></tbody></table>");
 for (var i = 0; i <= dati.length + i ;i++)
 {
- alert(i);
-$("#Tab").append("<tr value ='"+ i +"'><td>" + dati[0][i].Nome + "</td><td>" + dati[0][i].Posti + "</td><td>" + dati[0][i].Citta + "</td></tr>");
-// $("#sel").append("<option value='r" + dati[0][i].Citta +"'>" + dati[0][i] + " </option>");
+  $("#Tab").append("<tbody align='center'><tr value ='"+ i +"'><td>" + dati[0][i].Nome + "</td><td>" + dati[0][i].Posti + "</td><td>" + dati[0][i].Citta + "</tbody></td></tr>"); 
+  // $("#sel").append("<option value='r" + dati[0][i].Citta +"'>" + dati[0][i] + " </option>");
 }
+
  });
 });  
 });
